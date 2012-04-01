8a4d4f714f82d4333703aad5a205973b7adfa901
pawel <pawel.samysev@gmail.com>
2012-04-01 13:57:46 +0400

    Some optimization

--- index.php
+++ index.php
@@ -14,30 +14,35 @@
 <hr>
 <hr>
 <tr>
-<td align='left' valign='top' width=15%>
+<td align='left' valign='top' width=20%>
 <H3> Рейсы:</h3>
 <form action='index.php' method='get'>
 <H2>Показать за:</h2>
-За час <input type='radio' name='reis' value='unfull' <?if($_REQUEST['reis']=='unfull'){echo "checked";}?>>
-Вообще все <input type='radio' name='reis' value='all' <?if($_REQUEST['reis']=='all'){echo "checked";}?>><br>
-Все оставшиеся за день <input type='radio' name='reis' value='full' <?if($_REQUEST['reis']=='full'){echo "checked";}?>>
-<button name='radio' value='1'>Долгопрудная - Москва</button>
-<button name='radio1' value='1'>Москва - Долгопрудная</button>
+<? 
+$sTimeType = $_REQUEST['reis'];
+$sTimeType = $sTimeType ? $sTimeType : 'unfull' ;
+$iPathType = $_REQUEST['path'];
+$iPathType = $iPathType ? $iPathType : 1;
+?>
+За час <input type='radio' name='reis' value='unfull' <?if($sTimeType=='unfull'){echo "checked";}?>>
+Вообще все <input type='radio' name='reis' value='all' <?if($sTimeType=='all'){echo "checked";}?>><br>
+Все оставшиеся за день <input type='radio' name='reis' value='full' <?if($sTimeType=='full'){echo "checked";}?>>
+<button name='path' value='1'>Долгопрудная - Москва</button>
+<button name='path' value='-1'>Москва - Долгопрудная</button>
 </form>
 <td align='center' width=85%>
 <?
 	include "connect.php";
-	if($_REQUEST['radio']==1){
-		$query1="drop view if exists temp; create view temp(reis_name, reis_number, start_date, end_date, cur) as(SELECT reis_name, reis_number, time(start_date), time(end_date), timediff(start_date, current_time)from main where type_of_reis=1);";}
-	else if($_REQUEST['radio1']==1){
-		$query1="drop view if exists temp; create view temp(reis_name, reis_number, start_date, end_date, cur) as(SELECT reis_name, reis_number, time(start_date), time(end_date), timediff(start_date, current_time)from main where type_of_reis=-1);";}
+    $query1 = "drop view if exists temp; create view temp(reis_name, reis_number, start_date, end_date, cur) as(SELECT reis_name, reis_number, time(start_date), time(end_date), timediff(start_date, current_time)from main where type_of_reis=".$iPathType.");";    
+    echo ' к '.$query1.' 3 ';
 	mysql_query($query1, $db);
-	if($_REQUEST['reis']=='unfull'){
-		$query="SELECT reis_name, reis_number, time(start_date), time(end_date), cur from temp where cur>0 and hour(cur)<1 order by cur;";}
-	else if($_REQUEST['reis']=='full'){
-		$query="SELECT reis_name, reis_number, time(start_date), time(end_date), cur from temp where cur>0 order by cur;";}
-	else if($_REQUEST['reis']=='all'){
-		$query="select reis_name, reis_number, time(start_date), time(end_date) from temp;";}
+	if($iPathType == 'unfull'){
+		$query = "SELECT reis_name, reis_number, time(start_date), time(end_date), cur from temp where cur>0 and hour(cur)<1 order by cur;";
+        } elseif($iPathType == 'full'){
+            $query = "SELECT reis_name, reis_number, time(start_date), time(end_date), cur from temp where cur>0 order by cur;";
+            } else {
+                $query = "select reis_name, reis_number, time(start_date), time(end_date) from temp;";
+            }
 	$result=mysql_query($query, $db);
 	
 	
@@ -53,16 +58,19 @@
 </tr>
 <?	$i=2;  
 	while ($line = mysql_fetch_array($result, MYSQL_ASSOC)):?>
-		<? if($i%2==0):echo '<tr bgcolor="#FFFF14">'; else: echo '<tr bgcolor="#52FFA8">';?>
-		<?endif?>
-     			<?foreach ($line as $col_value):?>
-            			<td>
-                		<? echo $col_value;?>
-            			</td>
-        		<?endforeach?>
-        	</tr>
-	<?$i=$i+1;?>
-    	<?endwhile?>
+		<? if ($i%2==0) {
+            echo '<tr bgcolor="#FFFF14">';
+        } else {
+            echo '<tr bgcolor="#52FFA8">';
+        }
+        foreach ($line as $col_value):?>
+            <td>
+                <? echo $col_value;?>
+            </td>
+        <?endforeach?>
+</tr>
+        <?$i++;?>
+    <?endwhile?>
 </table>
 
 <?
